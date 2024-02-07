a53f950fca74c728f6f07e16dc74330f25a83012
Nigel James <Nigel.James@squarecloud.com.au>
2024-02-07 20:05:11 +1100

    adding the zero sequence

--- doc/search-index.js
+++ doc/search-index.js
@@ -1,5 +1,5 @@
 var searchIndex = JSON.parse('{\
-"lib":{"doc":"Integer Sequences","t":"FF","n":["a000045","add"],"q":[[0,"lib"]],"d":["return the fibonacci at position index",""],"i":[0,0],"f":[[1,1],[[1,1],1]],"c":[],"p":[[15,"usize"]]}\
+"lib":{"doc":"Integer Sequences","t":"FFFF","n":["a000004","a000045","a000045_cf","add"],"q":[[0,"lib"]],"d":["return Zero","return the fibonacci at position index","return the fibonacci at position index using the closed …",""],"i":[0,0,0,0],"f":[[1,1],[1,1],[1,1],[[1,1],1]],"c":[],"p":[[15,"usize"]]}\
 }');
 if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
 if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,14 @@
 //!
 //! [`The Online Encyclopedia of Integer Sequences`]: https://oeis.org/
 
+/// return Zero
+///
+/// see [https://oeis.org/A000004](https://oeis.org/A000004)
+pub fn a000004(index: usize) -> usize {
+    return 0;
+}
+
+
 /// return the fibonacci at position index
 ///
 /// see [https://oeis.org/A000045](https://oeis.org/A000045)
@@ -15,6 +23,17 @@ pub fn a000045(index: usize) -> usize {
         _ =>  a000045(index - 2 ) + a000045( index - 1),
     }
 }
+
+///  return the fibonacci at position index using the closed form rather than recursion
+///
+/// see [a000045]
+/// Wikipedia [Closed Form Value](https://en.wikipedia.org/wiki/Fibonacci_sequence#Relation_to_the_golden_ratio)
+
+pub fn a000045_cf(index: usize) -> usize {
+
+
+}
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
